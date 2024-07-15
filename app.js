83bf77b0a32fde0a1eac735c67cc3eb19f9a2cf6
Edgardo Valencia <edgardo.avalencia@gmail.com>
2024-07-15 12:06:51 -0600

    Cors added

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import morgan from "morgan"
 import "dotenv/config";
 import "./src/db.js";
 import cookieParser from "cookie-parser"
+import cors from "cors"
 
 import authRoutes from "./src/routes/auth.route.js"
 import tasksRoutes from "./src/routes/tasks.routes.js";
@@ -10,9 +11,16 @@ import tasksRoutes from "./src/routes/tasks.routes.js";
 
 
 const app = express()
+app.use(cors({
+    origin: 'http://localhost:5173'
+}))
 app.use(express.json())
 app.use(cookieParser()) // This is used to read the cookies, workds similar like express,json() but this one is not build in
 //To install cookie parser just add via || npm i cookie-parser||
+
+
+
+
 
 app.use("/api/v1", authRoutes)
 app.use("/api/v1", tasksRoutes)
